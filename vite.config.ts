5d9206290cba3e37c17936aa6385ddfb0093c5f4
Bati <no-reply@batijs.dev>
2025-02-07 16:36:33 +0200

    scaffold Vike app with Bati

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import tailwindcss from "@tailwindcss/vite";
+import vikeSolid from "vike-solid/vite";
+import devServer from "@hono/vite-dev-server";
+import { defineConfig } from "vite";
+import vike from "vike/plugin";
+
+export default defineConfig({
+  plugins: [
+    vike({}),
+    devServer({
+      entry: "hono-entry.ts",
+
+      exclude: [
+        /^\/@.+$/,
+        /.*\.(ts|tsx|vue)($|\?)/,
+        /.*\.(s?css|less)($|\?)/,
+        /^\/favicon\.ico$/,
+        /.*\.(svg|png)($|\?)/,
+        /^\/(public|assets|static)\/.+/,
+        /^\/node_modules\/.*/,
+      ],
+
+      injectClientScript: false,
+    }),
+    vikeSolid(),
+    tailwindcss(),
+  ],
+  build: {
+    target: "es2022",
+  },
+});
